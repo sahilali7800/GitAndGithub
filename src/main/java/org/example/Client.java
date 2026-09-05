eb77ca1df37d06f6164d48fdc37cd93edff2ba78
sahilali7800 <sahilali321211@gmail.com>
2026-09-05 15:57:26 +0530

    Version 2

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -4,5 +4,6 @@ public class Client {
     static void main(String[] args) {
         System.out.println("Version 2");
         System.out.println("Abc");
+        System.out.println("XYZ");
     }
 }
